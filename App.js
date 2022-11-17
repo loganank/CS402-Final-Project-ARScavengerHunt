8057da31dd584eb2f3a8e3ad2c4582656fdc7b81
loganank <loganankarberg@u.boisestate.edu>
2022-11-17 13:35:00 -0700

    added arkit

--- App.js
+++ App.js
@@ -1,24 +1,47 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SelectScreen from './screens/SelectScreen';
+import { PermissionsAndroid } from 'react-native';
+import { Camera } from 'expo-camera';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Whatever</Text>
-    </View>
-  );
-}
+//import ARScreen from './screens/ARScreen';
+
+//ANDROID PERMISSIONS
+const requestCameraPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: "ARScavenger Hunt Camera Permission",
+        message:
+          "ARScavenger Hunt needs access to your camera " +
+          "so you can take pictures.",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK"
+      }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("You can use the camera");
+    } else {
+      console.log("Camera permission denied");
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  requestCameraPermission();
+
   return (
-    <NavigationContainer>
+    <NavigationContainer initialRouteName="AR">
       <Stack.Navigator>
         <Stack.Screen name="Home" component={SelectScreen} />
+        <Stack.Screen name="AR" component={ARScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
